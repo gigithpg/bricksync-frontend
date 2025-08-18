7f0e2550a9950e4ec07cd0a186e0e9ac3b515e95
gigithpg <gigithpg@gmail.com>
2025-08-18 10:13:55 +0530

    Fixed Vite build, moved chart.min.js to public, ensured styles in production

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,11 @@ export default defineConfig({
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html')
+      }
+    }
   },
   server: {
     port: 8080,
